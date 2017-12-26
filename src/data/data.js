a0afdd9cc75509ec61205b2f359d6a1dedcfcddc
lollipop7 <juanlilu@yeah.net>
2017-12-26 13:55:16 +0800

    init framework7 config

---
+++ src/data/data.js
@@ -0,0 +1,12 @@
+/**
+ * Created by lollipop on 2017/12/26
+ */
+module.exports = {
+    navName: [
+        {name: '简历详情'},
+        {name: '面试邀请'},
+        {name: '企业详情'},
+        {name: '职位详情'},
+        {name: '资讯详情'}
+    ]
+}
