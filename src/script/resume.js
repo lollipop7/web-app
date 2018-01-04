b47e8f7245e2bafb2f3ee68856c569bdd4cf5c5c
yangdoubleming <yangdoubleming@163.com>
2018-01-04 14:41:24 +0800

    简历详情 getCookie

--- src/script/resume.js
+++ src/script/resume.js
@@ -8,18 +8,18 @@ require.config({
     paths: {
         "jquery": "jquery.min",
         "lodash": "lodash.min",
-        "getCookie": "getCookie",
-        "getUrlParam": "getUrlParam"
+        "bundle": "bundle"
     }
 });
-require(['jquery', 'lodash', 'getCookie', 'getUrlParam'], function ($, _, getCookie, getUrlParam) {
+require(['jquery', 'lodash', 'bundle'], function ($, _, bundle) {
     // some code here
     document.cookie = "CHYJRTGN_APP1=" + escape("E6A77CE527FFAF63C3EEBF428C3351973A03FE6E4DCC27B49D42AF8AF096F2FE8CEC75826A9C5764772355A4C04E1BC3D92418E5361D7311E6FE89E5CC55F8ED80C803648A461B2305F27B6D0E8DCB959266B5888AE3BA00");//cookieName为要写入的Cookie的名称
     document.cookie = "CacheKey_APP1=" + escape("61502EADEDFE49FA84898F6A2855B402AF4FE59F91D05746FAD7C16455CC6E4475356FDEAC6A4E458B83A7648FF2B31C7BA718C124E06CC6");//cookieName为要写入的Cookie的名称
-    token = getCookie.getCookie("CHYJRTGN_APP1");
-    tokenKey = getCookie.getCookie("CacheKey_APP1");
-
-    console.log(token, tokenKey);
+    /**
+     获取企业版传过来的token 和tokenkey
+     */
+    token = bundle.getCookie("CHYJRTGN_APP1");
+    tokenKey = bundle.getCookie("CacheKey_APP1");
 
     var testJson = {
         "head": {
