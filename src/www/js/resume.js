c33ccef5c3cedf26cb8f79910baa88f50dd51ab3
lollipop7 <juanlilu@yeah.net>
2018-01-04 19:39:06 +0800

    resume.js变量结构赋值

--- src/www/js/resume.js
+++ src/www/js/resume.js
@@ -7,21 +7,24 @@
 require.config({
     paths: {
         "jquery": "jquery.min",
-        "lodash": "lodash.min",
-        "bundle": "bundle"
+        "bundle": "bundle",
+        "lodash": "lodash.min"
     }
 });
 require(['jquery', 'lodash', 'bundle'], function ($, _, bundle) {
     // some code here
-    document.cookie = "CHYJRTGN_APP1=" + escape("E6A77CE527FFAF63C3EEBF428C3351973A03FE6E4DCC27B49D42AF8AF096F2FE8CEC75826A9C5764772355A4C04E1BC3D92418E5361D7311E6FE89E5CC55F8ED80C803648A461B2305F27B6D0E8DCB959266B5888AE3BA00"); //cookieName为要写入的Cookie的名称
-    document.cookie = "CacheKey_APP1=" + escape("61502EADEDFE49FA84898F6A2855B402AF4FE59F91D05746FAD7C16455CC6E4475356FDEAC6A4E458B83A7648FF2B31C7BA718C124E06CC6"); //cookieName为要写入的Cookie的名称
+    //token
+    document.cookie = "CHYJRTGN_APP1=" + escape("E458BD3B260FB31485BB8BC07C9D8400741B232B2D53AF2CDC357750D3AD102C50C7B4E016D98C9E7EFD3DC5806B82F395279F5966B16340BFEF6CD725F1F332175D3ACF92A9941E7882C22EC2E02173875E0628FC03577D"); //cookieName为要写入的Cookie的名称
+    //tokenKey
+    document.cookie = "CacheKey_APP1=" + escape("61502EADEDFE49FA39A6B23F6B25ECCE22BC1C2AED9FB07EB99669FEFDA9EB83DEA97C7B906C81217253D740C37E8976E9A48D1ECD91D5C5"); //cookieName为要写入的Cookie的名称
+
     /**
      获取企业版传过来的token 和tokenkey
      */
     token = bundle.getCookie("CHYJRTGN_APP1");
     tokenKey = bundle.getCookie("CacheKey_APP1");
 
-    var testJson = {
+    const testJson = {
         "head": {
             "transcode": "H0016",
             "type": "h"
@@ -42,40 +45,72 @@ require(['jquery', 'lodash', 'bundle'], function ($, _, bundle) {
         data: JSON.stringify(testJson),
         dataType: "json",
         success: function (data) {
-            console.log(data);
-            if (data && data.resumebase) {
+            if (data.returnCode == "AAAAAAA") {
+                const {
+                    userid, //用户编号	
+                    username, //用户名称	
+                    salary, //薪资要求	
+                    livecityid, //城市	
+                    workyears, //工作年限	
+                    educationbg, //学历	
+                    headimg, //头像地址	
+                    titlenow, //目前职位	
+                    companynow, //目前公司	
+                    selfRemark, //个人评价		当前接口没有该数据
+                    joblb, //期望行业
+                    updatedate, //更新时间		更新时间
+                    phone, //手机号码		当islock为1时有数据，为0时没有数据
+                    email, //邮箱		当islock为1时有数据，为0时没有数据
+                    jobstatus, //工作状态		工作状态
+                    islock //是否查看该简历		0 未查看 1  以查看
+                } = data.resumebase; //简历基本信息
                 // 头部
                 $('.profile').css({
-                    'background-image': 'url(' + data.resumebase.headimg + ')',
+                    'background-image': 'url(' + headimg + ')',
                     'background-position': 'center'
                 });
-                $('#candiname').text(data.resumebase.username);
-                $('#candi-crop').text(data.resumebase.companynow);
-                $('#candi-posi').text(data.resumebase.titlenow);
-                $('.cur-sta span').text(data.resumebase.jobstatus);
-                $('.work-exp span').text(data.resumebase.workyears);
-                $('.location span').text(data.resumebase.livecityid);
-                $('.location span').text(data.resumebase.livecityid);
+                $('#candiname').text(username);
+                $('#candi-crop').text(companynow);
+                $('#candi-posi').text(titlenow);
+                $('.cur-sta span').text(jobstatus);
+                $('.work-exp span').text(workyears);
+                $('.location span').text(livecityid);
+                $('.location span').text(livecityid);
+
                 // 自我评价
-                $('.evalu-box').empty().append("<p>" + data.resumebase.selfRemark + "</p>");
+                $('.evalu-box').empty().append("<p>" + selfRemark + "</p>");
+
+                const {
+                    videodata, //简历视频信息
+                    workdata, //工作经历
+                    prodata, //项目经验
+                    edudata, //教育经历
+                    cerdata, //证书
+                    landata, //语言能力
+                    tradata, //培训能力
+                    othdata, //附加信息
+                    higdata //高级人才附加信息
+                } = data;
+
                 // 工作经验
-                var experReuslt = "";
-                JSON.parse(data.workdata).forEach(function (item, index) {
+                let experReuslt = "";
+                _.forEach(JSON.parse(workdata), function (item, index) {
                     experReuslt += `<div class='exper-box'><div class='exper-time'><span class='from'>${item.starttime}</span><b>-</b><span class='to'>${item.starttime}</span></div> <h3 class='corp-name'>${item.corpname}</h3><div class='job-posi'>${item.postcode}</div><div class='exper-content'><p>${item.workremark}</p></div></div>`;
                 });
                 $('.exper-box').remove();
                 $('#myexper').append(experReuslt);
+
                 // 教育经历
-                var eduReuslt = "";
-                JSON.parse(data.edudata).forEach(function (item, index) {
+                let eduReuslt = "";
+                _.forEach(JSON.parse(edudata), function (item, index) {
                     eduReuslt += `<div class='edu-box'><div class='school-name'>${item.school}</div><div class='edu-time'><span class='from'>${item.starttime}</span><b>-</b><span class='to'>${item.endtime}</span></div><div class='edu-info'><span class='profess'>${item.specialtyid}</span><b>|</b><span class='degree'>${item.educationbg}</span></div></div>`;
                 });
                 $('.edu-box').remove();
                 $('#myedu').append(eduReuslt);
+
                 // 证书
-                var cerReuslt = "";
-                console.log(JSON.parse(data.cerdata));
-                JSON.parse(data.cerdata).forEach(function (item, index) {
+                let cerReuslt = "";
+                JSON.parse(cerdata).forEach(function (item, index) {
                     cerReuslt += `<div class='credits-box'><div class='credits-name'>${item.certname}</div><div class='credits-time'><span class='from'>${item.gaintime}</span><span>颁发</span></div><div class='fromplace'><label>颁发机构</label><i>：</i><span>${item.certname}</span></div></div>`;
                 });
                 $('.credits-box').remove();
